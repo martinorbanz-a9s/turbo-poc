cfd7caf212efe8e94bf11447e2df604e00795166
martinorbanz-a9s <69192969+martinorbanz-a9s@users.noreply.github.com>
2023-04-26 13:49:13 +0200

    add ionic app

---
+++ apps/ionixt/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.anynines.ionixt',
+  appName: 'ionixt',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
